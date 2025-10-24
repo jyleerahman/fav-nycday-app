ea78e95c50575a2a96c336964b01650c5395d85c
JY Lee <jylee.rahman@gmail.com>
2025-10-24 18:36:22 -0400

    Add waypoints and markers

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,12 +4,33 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { SearchBox } from "@mapbox/search-js-react"
 import './App.css'
 
+type WayPoints = {
+  id: string,
+  name: string,
+  lng: number,
+  lat: number
+}
+
 function App() {
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
+  const mapRef = useRef<Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
   const [mapLoaded, setMapLoaded] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const [wayPoints, setWayPoints] = useState<WayPoints[]>([]);
   const accessToken = "pk.eyJ1IjoianlyYWhtYW4iLCJhIjoiY21oNHozb3NqMDI3ZjJycHU1N2JsazhtdiJ9.ho51ANPXxlvowesHLDv9Dg"
+
+  const markersRef = useRef<mapboxgl.Marker[]>([]);
+
+  function getwayPoints(res: any) {
+    const f = res.features[0];
+    const name = f.properties.name || "untitled";
+    const [lng, lat] = f.geometry.coordinates;
+
+    setWayPoints(prev => [
+      ...prev,
+      { id: crypto.randomUUID(), name, lng, lat }
+    ])
+  }
 
   useEffect(() => {
     mapboxgl.accessToken = accessToken
@@ -24,10 +45,31 @@ function App() {
       setMapLoaded(true);
     });
 
+    return () => {
+      markersRef.current.forEach(m => m.remove());
+      markersRef.current = [];
+      mapRef.current.remove()
+    }
   }, [])
+
+  useEffect(() => {
+    markersRef.current.forEach(m => m.remove())
+    markersRef.current = [];
+
+    wayPoints.forEach((point) => {
+      const marker = new mapboxgl.Marker()
+        .setLngLat([point.lng, point.lat])
+        .addTo(mapRef.current!);
+      markersRef.current.push(marker);
+    })
+
+  }, [mapLoaded, wayPoints])
+
 
   return (
     <>
+
+      <div>{JSON.stringify(wayPoints, null, 2)}</div>
       <SearchBox
         accessToken={accessToken}
         map={mapRef.current}
@@ -36,7 +78,8 @@ function App() {
         onChange={(d) => {
           setInputValue(d);
         }}
-        marker
+        onRetrieve={getwayPoints} // [lon,lat] 
+        marker={false}
       />
       <div id='map-container' ref={mapContainerRef} />
 

b4de54de7c47a1701eed2d562bf3076dc526193a
JY Lee <jylee.rahman@gmail.com>
2025-10-29 10:21:00 -0400

    subway tile bg

--- src/client/routes/Feed.tsx
+++ src/client/routes/Feed.tsx
@@ -3,7 +3,16 @@ import { useState, useRef } from "react"
 function Feed() {
 
     return (
-        <>hellooo</>
+        <>
+            <div className="tile-bg h-[4%]"></div>
+            <div className="light-green-tile-bg h-[3%]"></div>
+            <div className="dark-green-tile-bg h-[8%] "></div>
+            <div className="light-green-tile-bg h-[3%]"></div>
+            <div className="tile-bg h-[82%]">
+
+                <div className="bg-white h-[50px] w-[50px]"></div>
+            </div>
+        </>
     )
 }
 
